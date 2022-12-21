2f0e2aae272788d301524eb399432fa41bf25109
Daniel Barros <daniel.barros@chartboost.com>
2022-12-21 15:57:30 +0100

    Make configuration class compatible with Obj-C

--- Source/AmazonPublisherServicesAdapterConfiguration.swift
+++ Source/AmazonPublisherServicesAdapterConfiguration.swift
@@ -6,23 +6,23 @@
 import DTBiOSSDK
 
 /// A list of externally configurable properties pertaining to the partner SDK that can be retrieved and set by publishers.
-public class AmazonPublisherServicesAdapterConfiguration {
-
+@objc public class AmazonPublisherServicesAdapterConfiguration: NSObject {
+    
     /// Flag that can optionally be set to enable the partner's test mode.
     /// Disabled by default.
-    public static var testMode: Bool = false {
+    @objc public static var testMode: Bool = false {
         didSet {
             DTBAds.sharedInstance().testMode = testMode
+            print("Amazon Publishing Services SDK test mode set to \(testMode)")
         }
     }
     
     /// Flag that can optionally be set to enable the partner's verbose logging.
     /// Disabled by default.
-    public static var verboseLogging: Bool = false {
+    @objc public static var verboseLogging: Bool = false {
         didSet {
             DTBAds.sharedInstance().setLogLevel(DTBLogLevelAll)
+            print("Amazon Publishing Services SDK verbose logging set to \(verboseLogging)")
         }
     }
-    
-    /// Append any other properties that publishers can configure.
 }
