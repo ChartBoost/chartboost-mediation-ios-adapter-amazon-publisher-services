49358654d24953c38649e0b1c39c7b2f9e5696b8
Ray Graham <rayg@bitwisedesign.com>
2023-08-16 11:42:50 -0700

    Apply suggestions from code review

--- Source/AmazonPublisherServicesAdapterConfiguration.swift
+++ Source/AmazonPublisherServicesAdapterConfiguration.swift
@@ -9,7 +9,7 @@ import os.log
 /// A list of externally configurable properties pertaining to the partner SDK that can be retrieved and set by publishers.
 @objc public class AmazonPublisherServicesAdapterConfiguration: NSObject {
     
-    private static var log = OSLog(subsystem: "com.chartboost.mediation.adapter.amazon_aps", category: "Configuration")
+    private static let log = OSLog(subsystem: "com.chartboost.mediation.adapter.amazon_aps", category: "Configuration")
 
     /// Flag that can optionally be set to enable the partner's test mode.
     /// Disabled by default.
@@ -17,7 +17,7 @@ import os.log
         didSet {
             DTBAds.sharedInstance().testMode = testMode
             if #available(iOS 12.0, *) {
-                os_log(.debug, log: log, "Amazon Publishing Services SDK test mode set to %{public}s", testMode ? "true" : "false")
+                os_log(.debug, log: log, "Amazon Publishing Services SDK test mode set to %{public}s", "\(testMode)")
             }
         }
     }
@@ -28,7 +28,7 @@ import os.log
         didSet {
             DTBAds.sharedInstance().setLogLevel(DTBLogLevelAll)
             if #available(iOS 12.0, *) {
-                os_log(.debug, log: log, "Amazon Publishing Services SDK verbose logging set to %{public}s", verboseLogging ? "true" : "false")
+                os_log(.debug, log: log, "Amazon Publishing Services SDK verbose logging set to %{public}s", "\(verboseLogging)")
             }
         }
     }
