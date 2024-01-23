ede3678e492dcbc977fdbc87a689d086b159e354
Daniel Barros <daniel.barros@chartboost.com>
2024-01-23 14:42:29 -0800

    Add copyright notices

--- Source/AmazonPublisherServicesAdapterPreBiddingManager.swift
+++ Source/AmazonPublisherServicesAdapterPreBiddingManager.swift
@@ -1,3 +1,7 @@
+// Copyright 2022-2024 Chartboost, Inc.
+//
+// Use of this source code is governed by an MIT-style
+// license that can be found in the LICENSE file.
 
 import ChartboostMediationSDK
 import DTBiOSSDK
