ebfedefc49e550e1dece56079aafd5b5e9152778
Daniel Barros <daniel.barros@chartboost.com>
2024-04-18 11:22:46 +0530

    Fix linting issues

--- Source/AmazonPublisherServicesAdapterAd.swift
+++ Source/AmazonPublisherServicesAdapterAd.swift
@@ -18,11 +18,13 @@ class AmazonPublisherServicesAdapterAd: NSObject {
     var details: PartnerDetails = [:]
 
     /// The ad load request associated to the ad.
-    /// It should be the one provided on ``PartnerAdapter/makeBannerAd(request:delegate:)`` or ``PartnerAdapter/makeFullscreenAd(request:delegate:)``.
+    /// It should be the one provided on ``PartnerAdapter/makeBannerAd(request:delegate:)``
+    /// or ``PartnerAdapter/makeFullscreenAd(request:delegate:)``.
     let request: PartnerAdLoadRequest
     
     /// The partner ad delegate to send ad life-cycle events to.
-    /// It should be the one provided on ``PartnerAdapter/makeBannerAd(request:delegate:)`` or ``PartnerAdapter/makeFullscreenAd(request:delegate:)``.
+    /// It should be the one provided on ``PartnerAdapter/makeBannerAd(request:delegate:)``
+    /// or ``PartnerAdapter/makeFullscreenAd(request:delegate:)``.
     weak var delegate: PartnerAdDelegate?
 
     /// The partner adapter with a concrete type which can be used by the ad to obtain consent info.
