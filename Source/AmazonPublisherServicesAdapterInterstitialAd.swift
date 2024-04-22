4b91f3893f51d736c245d2e9402f642e249b46d1
Daniel Barros <dani.barros@me.com>
2024-04-22 23:35:20 +0530

    Split PartnerAd into banner and fullscreen (#65)
    * Split makeAd functions
    
    * Update show method
    
    * Update banners conformance
    
    * Fix linting issues
    
    * Fix indentation

--- Source/AmazonPublisherServicesAdapterInterstitialAd.swift
+++ Source/AmazonPublisherServicesAdapterInterstitialAd.swift
@@ -8,16 +8,7 @@ import DTBiOSSDK
 import Foundation
 
 /// The Chartboost Mediation Amazon Publisher Services adapter interstitial ad.
-final class AmazonPublisherServicesAdapterInterstitialAd: AmazonPublisherServicesAdapterAd, PartnerAd {
-    
-    /// The partner ad view to display inline. E.g. a banner view.
-    /// Should be nil for full-screen ads.
-    var inlineView: UIView? { nil }
-
-    /// The loaded partner ad banner size.
-    /// Should be `nil` for full-screen ads.
-    var bannerSize: PartnerBannerSize? { nil }
-
+final class AmazonPublisherServicesAdapterInterstitialAd: AmazonPublisherServicesAdapterAd, PartnerFullscreenAd {
     /// The APS ad dispatcher instance used to load an ad. We have strong reference here to keep it alive while the loading is ongoing.
     private var adLoader: DTBAdInterstitialDispatcher?
     
@@ -53,7 +44,7 @@ final class AmazonPublisherServicesAdapterInterstitialAd: AmazonPublisherService
     }
     
     /// Shows a loaded ad.
-    /// It will never get called for banner ads. You may leave the implementation blank for that ad format.
+    /// Chartboost Mediation SDK will always call this method from the main thread.
     /// - parameter viewController: The view controller on which the ad will be presented on.
     /// - parameter completion: Closure to be performed once the ad has been shown.
     func show(with viewController: UIViewController, completion: @escaping (Result<PartnerDetails, Error>) -> Void) {
