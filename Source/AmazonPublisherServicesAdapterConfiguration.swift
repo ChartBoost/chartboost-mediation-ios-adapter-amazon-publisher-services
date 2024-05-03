80acd0861ea5641709080a7d6f68928d32e92eab
Alex Rice <alexander.rice@chartboost.com>
2024-05-03 15:16:12 -0700

    HB-7607: Replace duplicated code with call to helper method (#68)
    * Use BannerSize.largestStandardFixedSizeThatFits
    
    * Make adapter version match podspec

--- Source/AmazonPublisherServicesAdapterConfiguration.swift
+++ Source/AmazonPublisherServicesAdapterConfiguration.swift
@@ -17,7 +17,7 @@ import DTBiOSSDK
     /// The version of the adapter.
     /// It should have either 5 or 6 digits separated by periods, where the first digit is Chartboost Mediation SDK's major version, the last digit is the adapter's build version, and intermediate digits are the partner SDK's version.
     /// Format: `<Chartboost Mediation major version>.<Partner major version>.<Partner minor version>.<Partner patch version>.<Partner build version>.<Adapter build version>` where `.<Partner build version>` is optional.
-    @objc public static let adapterVersion = "4.4.9.0.1"
+    @objc public static let adapterVersion = "5.4.9.0.1"
 
     /// The partner's unique identifier.
     @objc public static let partnerID = "amazon_aps"
